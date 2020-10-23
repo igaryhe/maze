0d152d1c1029cbca3f95f3426c5db88b79d1353b
Dan <igaryhe@gmail.com>
2020-10-23 19:27:54 +0800

    Noise

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public Sound[] sounds;
+    public Sound[] footstep;
+
+    private void Awake()
+    {
+        // base.Awake();
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = true;
+        }
+    }
+
+    public void Play(int n)
+    {
+        sounds[n].source.Play();
+    }
+
+    public void Stop(int n)
+    {
+        sounds[n].source.Stop();
+    }
+
+    private void Start()
+    {
+        // Play(0);
+        // Play(1);
+        // Play(2);
+    }
+}
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
     public GameObject spotlight;
+    public int noiseCount;
+
+    private void Start()
+    {
+        AudioManager.Instance.Play(noiseCount);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             // spotlight.SetActive(true);
             spotlight.GetComponent<Animator>().SetBool("lightOn", true);
+            AudioManager.Instance.Stop(noiseCount);
         }
     }
 
